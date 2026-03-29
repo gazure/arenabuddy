c11782e30f1e7e38bf5f3cd214885a83257310cc
Grant Azure <azure.grant@gmail.com>
2026-03-28 20:29:56 -0700

    refactor: make instrumentation idiomatic

--- arenabuddy/src/main.rs
+++ arenabuddy/src/main.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    arenabuddy::launch()?;
+    let app_name = std::env::var("APP_NAME").unwrap_or_else(|_| "arenabuddy".to_string());
+    arenabuddy::launch(app_name)?;
     Ok(())
 }
